6fc5eb4c23046935f74f268bbdc756de7975742c
PetusoTwo <1566258@senati.pe>
2025-06-11 18:54:17 -0500

    final configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,12 @@
 module com.example.labprofessoroak {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
+    
     requires com.fasterxml.jackson.databind;
     requires java.net.http;
 
     opens com.example.labprofessoroak to javafx.fxml;
+    opens com.example.labprofessoroak.controller to javafx.fxml;
     exports com.example.labprofessoroak;
     exports com.example.labprofessoroak.controller;
-    opens com.example.labprofessoroak.controller to javafx.fxml;
 }
